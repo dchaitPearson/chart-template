75955ceb5e4ef4e376ddfa8589ac7f452c47620a
geethaka <kasun.geethaka@icloud.com>
2018-11-05 10:06:56 -0700

    Added minikube custom script

--- travis/start_minikube.sh
+++ travis/start_minikube.sh
@@ -40,23 +40,4 @@ fi
 sleep 10
 
 source $(pwd)/travis/ecr/minikube-login.sh
-
-sudo -E minikube addons disable kube-dns
-sudo -E minikube addons disable coredns
-sudo -E minikube addons disable dashboard
-sudo -E minikube addons disable storage-provisioner
-sudo -E minikube addons disable nvidia-driver-installer
-sudo -E minikube addons disable nvidia-gpu-device-plugin
-sudo -E minikube addons disable default-storageclass
-sudo -E minikube addons disable addon-manager
-sudo -E minikube addons list
-
-echo "kubectl label nodes --all role=minion"
-kubectl label nodes --all role=minion
-echo "kubectl get nodes --show-labels"
-kubectl get nodes --show-labels
-# temp fix for kubernetes 1.10 with minikube
-echo "kubectl delete deployment kube-dns -n kube-system"
-kubectl delete deployment kube-dns -n kube-system
-kubectl delete sa kube-dns -n kube-system
-kubectl delete clusterroles system:kube-dns -n kube-system
+travis/minikube_custom.sh
